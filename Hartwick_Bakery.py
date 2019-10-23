fd103af0cb323a406af8184e7390e8cd5a8ac972
Isaac Waide <waidei@hartwick.edu>
2019-10-23 10:17:11 -0400

    update 23, 2019

---
+++ Hartwick_Bakery.py
@@ -0,0 +1,36 @@
+#Isaac Waide
+#October 14, 2019
+#Hartwick Bakery
+
+cookies = []
+candy = []
+
+def cookie_input():
+    for ele in range(0, 6):
+        value = int(input("Enter value for cookies sales"))
+        cookies.append(value)
+sum = 0
+for num in input:
+    sum = sum +input
+
+average = sum / input
+print("The sum of the Cookies is: ", sum)
+print("The Average of the Sales on Cookies is: ", average)
+
+
+def candy_input():
+    for ele in range(0, 6):
+        value = int(input("Enter value for candy sales"))
+        candy.append(value)
+
+sum = 0
+for num in input:
+    sum = sum +input
+
+average = sum / input
+print("The sum of the Candy is: ", sum)
+print("The Average of the Sales on Candy is: ", average)
+
+
+
+
